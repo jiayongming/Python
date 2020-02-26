cafc9f6c1f815eb9bde87efb71543b5a6af70a74
ymjia <ymjia@rxhui.com>
2020-02-26 09:55:38 +0800

    郑州在售楼盘爬虫（真二网）

--- 爬虫集合/zz_zhen2_xinfang.py
+++ 爬虫集合/zz_zhen2_xinfang.py
@@ -8,6 +8,15 @@ excel_name = "zz_zhen2_xinfang.xlsx"
 wb = Workbook()
 ws1 = wb.active
 ws1.title = '郑州在售楼盘(真二网)'
+ws1['A1'] = '小区名称'
+ws1['B1'] = '在售状态'
+ws1['C1'] = '行政区域'
+ws1['D1'] = '具体位置'
+ws1['E1'] = '在售户型'
+ws1['F1'] = '建筑面积(平)'
+ws1['G1'] = '单价(元/平)'
+ws1['H1'] = '房屋标签'
+ws1['I1'] = '详情链接'
 
 def download_page(url):
     '''
@@ -26,31 +35,56 @@ def get_text_list(html, page_num):
     soup = BeautifulSoup(html, 'html.parser')
     text_list = soup.find_all('li', class_='search_item')
     for i in text_list:
-        info = i.find('div', class_='search_info')
-        search_url = 'https://www.zhen22.com' + info.find('div', class_='item_content').find('div', class_='item_left').find('div', class_='search_title').find('a')['href']
-        search_status = info.find('div', class_='item_content').find('div', class_='item_left').find('div', class_='search_title').find('a').find('el-tag').get_text()
-        search_name = info.find('div', class_='item_content').find('div', class_='item_left').find('div', class_='search_title').find('a').get_text().replace(search_status, '').replace('\n', '').strip()
-        search_miaosu = info.find('div', class_='item_content').find('div', class_='item_left').find('div', class_='search_rooms').find('div', class_='area').get_text().replace('\n', '').strip()
-        search_huxing = info.find('div', class_='item_content').find('div', class_='item_left').find('div', class_='search_rooms').find('div', class_='search_rooms_main').get_text().replace('\n', '').replace(' ', '').strip()
-        dizhis = info.find('div', class_='item_content').find('div', class_='item_left').find('div', class_='search_address').find('div', class_='search_address_main').get_text().replace('\n', '').replace(' ', '').strip().split(']')
+        content = i.find('div', class_='search_info').find('div', class_='item_content')
+        left = content.find('div', class_='item_left')
+        right = content.find('div', class_='item_right')
+
+        search_url = 'https://www.zhen22.com' + left.find('div', class_='search_title').find('a')['href']
+
+        search_status = left.find('div', class_='search_title').find('a').find('el-tag')
+        if search_status is None:
+            search_status = ''
+        else:
+            search_status = search_status.get_text()
+
+        search_name = left.find('div', class_='search_title').find('a').get_text().replace(search_status, '').replace('\n', '').strip()
+
+        search_miaosu = left.find('div', class_='search_rooms').find('div', class_='area')
+        if search_miaosu is None:
+            search_miaosu = ''
+        else:
+            search_miaosu = search_miaosu.get_text().replace('\n', '').replace('建面：', '').replace('㎡', '').strip()
+
+        search_huxing = left.find('div', class_='search_rooms').find('div', class_='search_rooms_main')
+        if search_huxing is None:
+            search_huxing = ''
+        else:
+            search_huxing = search_huxing.get_text().replace('\n', '').replace(' ', '').replace('户型：', '').strip()
+
+        dizhis = left.find('div', class_='search_address').find('div', class_='search_address_main').get_text().replace('\n', '').replace(' ', '').strip().split(']')
 
         search_quyu = dizhis[0].replace('[', '')
         search_dizhi = dizhis[1]
 
-        print(search_url)
+        search_price = right.find('div', class_='hasPrice')
+        if search_price is None:
+            search_price = ''
+        else:
+            search_price = search_price.get_text().replace('均价', '').replace('元/㎡', '')
 
-        ws1['A1'] = '小区名称'
-        ws1['B1'] = '小区性质'
-        ws1['C1'] = '小区状态'
-        ws1['D1'] = '行政区域'
-        ws1['E1'] = '区域板块'
-        ws1['F1'] = '具体位置'
-        ws1['G1'] = '在售户型'
-        ws1['H1'] = '建筑面积'
-        ws1['I1'] = '房屋总价(万元)'
-        ws1['J1'] = '房屋单价(元/平均价)'
-        ws1['K1'] = '房屋特色'
-        ws1['L1'] = '详情链接'
+        search_tags = i.find('div', class_='search_info').find('div', class_='search_tags').get_text().replace('\n','/').strip('/')
+
+        index = (text_list.index(i) + 2) + (page_num * 10)
+        ws1['A%s' % index] = search_name
+        ws1['B%s' % index] = search_status
+        ws1['C%s' % index] = search_quyu
+        ws1['D%s' % index] = search_dizhi
+        ws1['E%s' % index] = search_huxing
+        ws1['F%s' % index] = search_miaosu
+        ws1['G%s' % index] = search_price
+        ws1['H%s' % index] = search_tags
+        ws1['I%s' % index] = search_url
+
         wb.save(filename=excel_name)
 
 def create_dir(name):
@@ -66,7 +100,7 @@ def execute(url, page_num):
 def main():
     create_dir('lianjia')
     threads = []
-    queue = [i for i in range(1, 2)]  # 构造 url 链接 页码。
+    queue = [i for i in range(1, 147)]  # 构造 url 链接 页码。
     while len(queue) > 0:
         for thread in threads:
             if not thread.is_alive():
